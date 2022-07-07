75a38fbbebe801c78051777eac364717c8864bb7
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-07-06 23:35:01 -0400

    refactor: organização das rotas da aplicação.

--- src/applicationBootstrap.ts
+++ src/applicationBootstrap.ts
@@ -1,13 +1,10 @@
 import express from "express";
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specifications.routes";
+import { router } from "./routes";
 
 const applicationBootstrap = express();
 
 applicationBootstrap.use(express.json());
-
-applicationBootstrap.use('/categories', categoriesRoutes);
-applicationBootstrap.use('/specifications', specificationsRoutes);
+applicationBootstrap.use(router);
 
 applicationBootstrap.listen(3333, () => {
   console.log('Aplication started on port: 3333 🚀')
