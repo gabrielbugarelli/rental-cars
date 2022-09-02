f76424a31d9f58a1bf223cd12d862cb07ca9a5cb
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-09-02 10:08:05 -0400

    feat: implementação da rota de upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,12 @@
 import { Router, Request, Response } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
+import multer from 'multer';
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: './tmp'
+});
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +14,13 @@ categoriesRoutes.post('/', (request: Request, response: Response) => {
 
 categoriesRoutes.get('/', (request: Request, response: Response) => {
   return listCategoryController.handle(request, response);
-})
+});
+
+categoriesRoutes.post('/import', upload.single("file"), (request: Request, response: Response) => {
+  const { file } = request;
+  console.log(file);
+  
+  return response.send();
+});
 
 export { categoriesRoutes };
