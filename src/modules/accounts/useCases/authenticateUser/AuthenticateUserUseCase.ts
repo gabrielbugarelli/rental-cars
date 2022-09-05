3499eb3e3083751242eea3f07d5249935cb75e16
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-09-05 04:32:15 -0400

    feat: implementação do serviço de autenticação de usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,55 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../respositories/contracts/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+export class AuthenticateUserUseCase {
+  private usersRepository: IUsersRepository;
+
+  constructor(@inject('UsersRepository') usersRepository: IUsersRepository) {
+    this.usersRepository = usersRepository;
+  };
+
+  async execute({email, password}: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    
+    if(!user) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const token = sign({}, '3272357538782F413F4428472D4B6150', {
+      subject: user.id,
+      expiresIn: '1d'
+    });
+
+    let userTokenResponse: IResponse = {
+      user: {
+        name: user.name,
+        email: user.email
+      },
+      token
+    }
+
+    return userTokenResponse;
+  }
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { usersRoutes } from "./users.routes";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+import { authenticateRoutes } from "./authenticate.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
