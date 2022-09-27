ce9dbcff172a753dde12692825108e11aa10a8f3
Gabriel <gabrielfonsecaoficial@gmail.com>
2022-09-27 16:09:15 -0400

    fix: ajuste no request param para capturar o user id.

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -4,13 +4,13 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 export class UpdateUserAvatarController {
   async handle(request: Request, respons: Response) {
-    const { id } = request.user;
+    const id = request.user.id
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
     const avatar_file = request.file.filename;
 
-    await updateUserAvatarUseCase.execute({user_id: id, avatar_file});
+    await updateUserAvatarUseCase.execute({ user_id: id, avatar_file });
 
-    response.status(204).send({updated: true});
+    response.status(204).send({ updated: true });
   }
 }
